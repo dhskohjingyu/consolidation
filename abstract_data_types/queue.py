f30a597303c567cd0a7cfca9ffb0c443bcf3b24d
dhskohjingyu <koh.jingyu@dhs.sg>
2013-05-09 15:11:28 +0800

    Create queue.py
    Python file with class Queue to emulate a queue data structure.

---
+++ abstract_data_types/queue.py
@@ -0,0 +1,21 @@
+class Queue():
+    def __init__(self):
+        self.__items = []
+
+    def enqueue(item):
+        self.__items.append(item)
+
+    def dequeue():
+        self.__items.pop(-1)
+
+    def display():
+        print(self.__items)
+
+q = Queue()
+q.display()
+q.enqueue(5)
+q.enqueue(3)
+q.enqueue(2)
+q.display()
+d.dequeue()
+q.display()
