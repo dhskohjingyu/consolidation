ca78c3755fdf383ee44d660322bb3768e5e749a4
dhskohjingyu <koh.jingyu@dhs.sg>
2013-10-01 22:41:22 +0800

    Update linked_list.py

--- abstract_data_types/linked_list.py
+++ abstract_data_types/linked_list.py
@@ -1,79 +1,58 @@
-class LinkedNode():
+class LinkedNode(object):
     def __init__(self, value):
-        self.__value = value
-        self.__pointer = None
+        self.value = value
+        self.pointer = None
 
-    def set_value(self, new_value):
-        self.__value = new_value
-
-    def get_value(self):
-        return self.__value
-
-    def set_pointer(self, new_pointer):
-        self.__pointer = new_pointer
-
-    def get_pointer(self):
-        return self.__pointer
-
-class LinkedList():
+class LinkedList(object):
     def __init__(self):
-        self.__head = None
-        self.__tail = None
+        self.head = None
 
-    def add_value(self, value):
-        new = LinkedNode(value)
-
-        if self.__head == None:
-            self.__head = new
-
-        if self.__tail != None:
-            self.__tail.set_pointer(new)
-
-        self.__tail = new
-
-    def remove_node(self, index):
-        node = self.__head
-        previous_node = None
-        count = 0
-
-        while count < index:
-            previous_node = node
-            node = node.get_pointer()
-            count += 1
-
-        if node == None:
-            print("IndexError: stack index out of range")
+    def insert(self, value):
+        if self.head == None:
+            self.head = LinkedNode(value)
         else:
-            next_node = node.get_pointer()
-            
-            if previous_node == None:
-                # removing the head
-                self.__head = next_node
-            else:
-                previous_node.set_pointer(next_node)
+            current_node = self.head
 
-            if next_node == None:
-                # removing the tail
-                self.__tail = previous_node
-            else:
-                previous_node.set_pointer(next_node)
+            # find the last node
+            while current_node.pointer:
+                current_node = current_node.pointer
+
+            current_node.pointer = LinkedNode(value)
+
+    def delete(self, value):
+        prev = None
+        current_node = self.head
+
+        while current_node:
+            if current_node.value == value:
+                # delete this
                 
-            node = None
+                if prev == None:
+                    # removing the head
+                    self.head = current_node.pointer
+                else:
+                    prev.pointer = current_node.pointer
+                    
+                current_node = None
+                break
+                
+            prev = current_node
+            current_node = current_node.pointer
 
-    def display(self):
-        node = self.__head
+    def print_list(self):
+        current_node = self.head
 
-        while node:
-            print(node.get_value())
-            node = node.get_pointer()
+        while current_node:
+            print(current_node.value, end=", ")
+            current_node = current_node.pointer
+            
+        print("") # newline for formatting
 
 l = LinkedList()
-l.add_value(2)
-l.add_value(3)
-l.add_value(4)
-l.add_value(5)
-l.add_value(6)
-l.display()
-print("--------")
-l.remove_node(2)
-l.display()
+l.insert(5)
+l.insert(7)
+l.insert(10)
+l.insert(12)
+l.print_list()
+l.delete(5)
+l.print_list()
